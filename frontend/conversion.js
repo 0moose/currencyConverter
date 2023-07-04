98f97d727a85a856163a94bc553fa93c7f9ef2d0
0moose <79241242+0moose@users.noreply.github.com>
2023-07-04 19:58:13 +0100

    Update conversion.js

--- frontend/conversion.js
+++ frontend/conversion.js
@@ -1,37 +1,3 @@
-// Json data for conversion rates
-const conversionRates = {
-  "USD": {
-    "EUR": 0.85,
-    "GBP": 0.72,
-    "CAD": 1.2,
-    "NGN": 1.65
-  },
-  "EUR": {
-    "USD": 1.18,
-    "GBP": 0.85,
-    "CAD": 1.44,
-    "NGN": 1.75
-  },
-  "GBP": {
-    "USD": 1.58,
-    "EUR": 0.65,
-    "CAD": 1.64,
-    "NGN": 2.05
-  },
-  "CAD": {
-    "USD": 1.98,
-    "GBP": 0.85,
-    "EUR": 1.86,
-    "NGN": 1.75
-  },
-  "NGN": {
-    "USD": 0.65,
-    "GBP": 0.75,
-    "CAD": 1.21,
-    "EUR": 0.75
-  }
-};
-
 // Populate the currency options in the select elements
 function populateCurrencyOptions() {
   const fromCurrencySelect = document.getElementById('fromCurrency');
@@ -48,36 +14,62 @@ function populateCurrencyOptions() {
   }
 }
 
+// Fetch exchange rates from the API
+async function fetchExchangeRates() {
+  const apiKey = '59d92f36991030fd0158570e'; //
+  const baseCurrency = 'USD'; // Replace with your desired base currency
+  const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCurrency}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.conversion_rates;
+  } catch (error) {
+    console.error('Failed to fetch exchange rates:', error);
+    return null;
+  }
+}
+
 // Convert the currency
-function convertCurrency() {
+async function convertCurrency() {
   // Step 1: Receive input
-  const amountInput = document.getElementById("amount").value;
-  const fromCurrencyInput = document.getElementById("fromCurrency").value;
-  const toCurrencyInput = document.getElementById("toCurrency").value;
+  const amountInput = document.getElementById('amount').value;
+  const fromCurrencyInput = document.getElementById('fromCurrency').value;
+  const toCurrencyInput = document.getElementById('toCurrency').value;
 
   // Step 2: Check if input is a valid number
   if (isNaN(amountInput)) {
-    document.getElementById("convertedAmount").value = "Invalid amount";
+    document.getElementById('convertedAmount').value = 'Invalid amount';
     return;
   }
 
-  // Step 3: Perform the currency conversion
-  const conversionRate = conversionRates[fromCurrencyInput][toCurrencyInput];
-  if (conversionRate === undefined) {
-    document.getElementById("convertedAmount").value = "Invalid conversion";
+  // Step 3: Fetch exchange rates from the API
+  const rates = await fetchExchangeRates();
+
+  if (rates === null) {
+    document.getElementById('convertedAmount').value = 'Failed to fetch rates';
     return;
   }
+
+  // Step 4: Perform the currency conversion
+  const conversionRate = rates[toCurrencyInput] / rates[fromCurrencyInput];
+
+  if (isNaN(conversionRate)) {
+    document.getElementById('convertedAmount').value = 'Invalid conversion';
+    return;
+  }
+
   const convertedAmount = amountInput * conversionRate;
 
-  // Step 4: Display the output
-  const outputElement = document.getElementById("convertedAmount");
+  // Step 5: Display the output
+  const outputElement = document.getElementById('convertedAmount');
   outputElement.value = convertedAmount.toFixed(2);
-  outputElement.style.display = "block";
+  outputElement.style.display = 'block';
 }
 
 // Trigger conversion when the button is clicked
-const convertButton = document.getElementById("convertButton");
-convertButton.addEventListener("click", convertCurrency);
+const convertButton = document.getElementById('convertButton');
+convertButton.addEventListener('click', convertCurrency);
 
 // Populate currency options when the page loads
-window.addEventListener("load", populateCurrencyOptions);
+window.addEventListener('load', populateCurrencyOptions);
